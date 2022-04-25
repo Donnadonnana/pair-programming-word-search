463cc6938091abae811e5ae1cd1389b49a3cf566
donnahuang <donnaphzj@gmail.com>
2022-04-25 15:25:52 -0400

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++){
+        for (arr of letters) {
+            if (verticalJoin[i] === undefined) {
+                verticalJoin[i] = ''
+            }
+            const letter = arr[i];
+            verticalJoin[i] = verticalJoin[i].concat(letter);
+        }
+    }
+    
+
+    console.log(verticalJoin);
+
+    
+
+    if (horizontalJoin.includes(word) || verticalJoin.includes(word)) {
+        return true
+    } else {
+        return false;
     }
 }
+ 
 
-module.exports = wordSearch
+    
+
+
+const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'FRANK')
+    
+    module.exports = wordSearch
